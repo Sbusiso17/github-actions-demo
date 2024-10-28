69bb64da75321b7c25ede164bd2bfe221e907b63
Sbusiso17 <152275698+Sbusiso17@users.noreply.github.com>
2024-10-29 00:43:07 +0200

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/androidfactory/githubactionsdemo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/androidfactory/githubactionsdemo/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.androidfactory.githubactionsdemo", appContext.packageName)
+        assertEquals("com.example.flutter_application_1", appContext.packageName)
     }
 }
